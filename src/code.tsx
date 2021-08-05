4ea6ed929c2df70fd65e533abbf726fe5e234d6e
Benny Yan <benny@yan.fm>
2021-08-05 12:23:35 -0500

    colors

--- src/code.tsx
+++ src/code.tsx
@@ -1,6 +1,11 @@
 import { setupMainThread } from "react-figma/rpc";
-import { parseToRgb } from "polished";
 import { flatten } from "lodash";
+import {
+  isJsonString,
+  createColors,
+  rmPrefixSlash,
+  MessageTypes,
+} from "./helpers";
 
 // This shows the HTML page in "ui.html".
 figma.showUI(__html__, { width: 300, height: 350 });
@@ -9,60 +14,6 @@ figma.showUI(__html__, { width: 300, height: 350 });
 
   {"functional":{"text":{"muted":"#727272","default":"#3c3c3c","dark":"#14151a","border":{"border-default":"#dbdfea","border-muted":"#eceef4","input":{"disabled-bg":"#f5f6f7","disabled-text":"#747474"}},"background":{"default":"#f9fafc"},"table":{"header":"#f9fafc"}},"icon":{"disabled":"rgba(67, 90, 111, 0.3)","default":"#727272"},"border":{"border-default":"#dbdfea","border-muted":"#eceef4"}}}
 */
-
-export enum MessageTypes {
-  MissingColors = "MissingColors",
-  InputColors = "InputColors",
-  Error = "Error",
-}
-
-const createSolidPaint = (color: string): SolidPaint => {
-  if (!color) return null;
-  const col = parseToRgb(color);
-  return {
-    color: {
-      r: col.red / 255,
-      g: col.green / 255,
-      b: col.blue / 255,
-    },
-    type: "SOLID",
-  };
-};
-
-/** recursive function to generate color objects in figma */
-const createColors = (key, val, path?: string): PaintStyle[] => {
-  if (typeof val === "string") {
-    try {
-      const style = figma.createPaintStyle();
-      const paint = createSolidPaint(val);
-      style.name = `${path || ""}/${key}`;
-      style.paints = [paint];
-      return [style];
-    } catch (e) {
-      console.log(e);
-      return;
-    }
-  }
-  return flatten(
-    Object.entries(val).map(([k, v]) =>
-      createColors(k, v, `${path || ""}/${key}`)
-    )
-  );
-};
-
-function isJsonString(str) {
-  try {
-    JSON.parse(str);
-  } catch (e) {
-    return false;
-  }
-  return true;
-}
-
-const rmPrefixSlash = (name: string): string => {
-  if (name[0] === "/") return name.substring(1);
-  return name;
-};
 
 const generateColors = ({ code }: { code: string }) => {
   try {
@@ -122,14 +73,8 @@ const generateColors = ({ code }: { code: string }) => {
   }
 };
 
-// const changeApi = (msg) => {
-//   fetch(msg.url)
-// };
-
 figma.ui.onmessage = (msg) => {
   switch (msg.type as MessageTypes) {
-    // case "change-api":
-    //   changeApi(msg);
     case MessageTypes.InputColors:
       generateColors(msg);
   }
---
+++ src/helpers.ts
@@ -0,0 +1,56 @@
+import { flatten } from "lodash";
+import { parseToRgb } from "polished";
+
+export enum MessageTypes {
+  MissingColors = "MissingColors",
+  InputColors = "InputColors",
+  Error = "Error",
+}
+
+export const createSolidPaint = (color: string): SolidPaint => {
+  if (!color) return null;
+  const col = parseToRgb(color);
+  return {
+    color: {
+      r: col.red / 255,
+      g: col.green / 255,
+      b: col.blue / 255,
+    },
+    type: "SOLID",
+  };
+};
+
+/** recursive function to generate color objects in figma */
+export const createColors = (key, val, path?: string): PaintStyle[] => {
+  if (typeof val === "string") {
+    try {
+      const style = figma.createPaintStyle();
+      const paint = createSolidPaint(val);
+      style.name = `${path || ""}/${key}`;
+      style.paints = [paint];
+      return [style];
+    } catch (e) {
+      console.log(e);
+      return;
+    }
+  }
+  return flatten(
+    Object.entries(val).map(([k, v]) =>
+      createColors(k, v, `${path || ""}/${key}`)
+    )
+  );
+};
+
+export const isJsonString = (str) => {
+  try {
+    JSON.parse(str);
+  } catch (e) {
+    return false;
+  }
+  return true;
+};
+
+export const rmPrefixSlash = (name: string): string => {
+  if (name[0] === "/") return name.substring(1);
+  return name;
+};
